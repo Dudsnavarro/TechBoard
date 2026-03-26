b886f64a2d13c0c6cdc50211dd6ca5a0e98758d6
Dudsnavarro <eduardo.dudunavarro@oultook.com>
2026-03-26 09:14:51 -0300

    Card de eventos(incompleto)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,62 @@
 import "./App.css";
+import { Banner } from "./components/Banner";
+import { CardEvento } from "./components/CardEventos";
 import {FormularioDeEvento} from "./components/FormularioDeEvento";
+import { Tema } from "./components/Tema";
 
 function App() {
+
+const temas = [
+  {
+    id:1,
+    nome: 'front-end'
+  },
+  {
+    id:2,
+    nome: 'back-end'
+  },
+  {
+    id:3,
+    nome: 'devops'
+  },
+  {
+    id:4,
+    nome: 'inteligencia artificial'
+  },
+  {
+    id:5,
+    nome: 'data science'
+  },
+  {
+    id:6,
+    nome: 'cloud'
+  }
+]
+
+const eventos =[
+  {
+    capa: 'src/assets/Imagem1.png',
+    tema: temas[0],
+    date: new Date(),
+    titulo: 'Mulheres no Front'
+  }
+]
+
   return (
     <main>
-      
       <header>
         <img src="/Logo.png" alt="Logo do site techboard" />
       </header>
-
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
-
+      <Banner></Banner>
       <FormularioDeEvento />
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+            <CardEvento evento={eventos[0]} />
+          </section>
+        )
+      })}
     </main>
   );
 }
---
+++ src/components/CardEventos/index.jsx
@@ -0,0 +1,20 @@
+import './card-evento.css'
+
+export function CardEvento ({evento}) {
+    return (
+        <div className='card-evento'>
+            <img src={evento.capa} alt={evento.titulo} />
+            <div className='corpo'>
+                <p className="tag">
+                    {evento.tema.nome}
+                </p>
+                <p>
+                    {evento.data}
+                </p>
+                <h4 className='titulo'>
+                    {evento.titulo}
+                </h4>
+            </div>
+        </div>
+    )
+}
