c598edeaf068402046880d7551a4f75eddf15d99
Dudsnavarro <you@example.com>
2026-03-30 20:33:25 -0300

    Filtro de eventos por categorias

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,49 @@ import { useState } from "react";
 import "./App.css";
 import { Banner } from "./components/Banner";
 import { CardEvento } from "./components/CardEventos";
-import {FormularioDeEvento} from "./components/FormularioDeEvento";
+import { FormularioDeEvento } from "./components/FormularioDeEvento";
 import { Tema } from "./components/Tema";
 
 function App() {
+  const temas = [
+    {
+      id: 1,
+      nome: "front-end",
+    },
+    {
+      id: 2,
+      nome: "back-end",
+    },
+    {
+      id: 3,
+      nome: "devops",
+    },
+    {
+      id: 4,
+      nome: "inteligencia artificial",
+    },
+    {
+      id: 5,
+      nome: "data science",
+    },
+    {
+      id: 6,
+      nome: "cloud",
+    },
+  ];
 
-const temas = [
-  {
-    id:1,
-    nome: 'front-end'
-  },
-  {
-    id:2,
-    nome: 'back-end'
-  },
-  {
-    id:3,
-    nome: 'devops'
-  },
-  {
-    id:4,
-    nome: 'inteligencia artificial'
-  },
-  {
-    id:5,
-    nome: 'data science'
-  },
-  {
-    id:6,
-    nome: 'cloud'
+  const [eventos, setEventos] = useState([
+    {
+      capa: "https://alphaconvites.com.br/wp-content/uploads/2023/01/festa-antes-formatura-scaled-1.jpg",
+      tema: temas[0],
+      date: new Date(),
+      titulo: "Mulheres no Front",
+    },
+  ]);
+
+  function adicioarEvento(evento) {
+    setEventos([...eventos, evento]);
   }
-]
-
-const [eventos, setEventos] = useState ([
-  {
-    capa: 'https://alphaconvites.com.br/wp-content/uploads/2023/01/festa-antes-formatura-scaled-1.jpg',
-    tema: temas[0],
-    date: new Date(),
-    titulo: 'Mulheres no Front'
-  }
-])
-
-function adicioarEvento (evento) {
-  setEventos([...eventos, evento])
-}
 
   return (
     <main>
@@ -54,16 +53,30 @@ function adicioarEvento (evento) {
       </header>
       <Banner></Banner>
       <FormularioDeEvento temas={temas} aoSubmeter={adicioarEvento} />
-      {temas.map(function (item) {
-        return (
-          <section key={item.id}>
-            <Tema tema={item} />
-            {eventos.map(function (item, index) {
-              return <CardEvento evento={item} key={index}/>
-            })} 
-          </section>
-        )
-      })}
+      <section className="container">
+        {temas.map(function (tema) {
+          if (!eventos.some(function(evento){
+            return evento.tema.id === tema.id
+          })) {
+            return null
+          }
+
+          return (
+            <section key={tema.id}>
+              <Tema tema={tema} />
+              <div className="eventos">
+                {eventos
+                .filter(function (evento){
+                  return evento.tema.id === tema.id
+                })
+                .map(function (evento, index) {
+                  return <CardEvento evento={evento} key={index} />;
+                })}
+              </div>
+            </section>
+          );
+        })}
+      </section>
     </main>
   );
 }
